f0701d682c0f0e2cfcb23f1e8819e7af87ccc94e
Kyle Simmons <simmonskyleb@gmail.com>
2023-05-26 00:14:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const hbs = require('hbs');
 const { engine } = require('express-handlebars');
 const session = require('express-session');
 const path = require('path');
+const fs = require('fs');
 const notFoundMiddleware = require('./middleware/notFoundMiddleware');
 const errorMiddleware = require('./middleware/errorMiddleware');
+const { Sequelize } = require('sequelize');
 
 // Import routes
 const homeRoutes = require('./routes/home');
@@ -39,6 +41,22 @@ app.use(express.urlencoded({ extended: true }));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Read the schema file and execute the SQL statements
+const schemaPath = path.join(__dirname, 'db', 'schema.sql');
+const schema = fs.readFileSync(schemaPath, 'utf8');
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+});
+
+sequelize
+  .query(schema)
+  .then(() => {
+    console.log('Schema created successfully.');
+  })
+  .catch((error) => {
+    console.error('Error creating schema:', error);
+  });
 
 // Define routes
 app.use('/', homeRoutes);
