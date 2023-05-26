28406eed9f86fa39fbda2700edb9a0c7a69a7010
Kyle Simmons <simmonskyleb@gmail.com>
2023-05-26 00:02:15 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,20 @@
-require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // Database connection from Heroku's environment variable
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'mysql',
+    protocol: 'mysql',
+    logging: true // Set to true to see database queries in the console
+  });
+} else {
+  // Local database connection
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+  });
+}
 
 module.exports = sequelize;
